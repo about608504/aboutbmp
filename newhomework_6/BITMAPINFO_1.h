ad006c6580ed84e4f243a7515345c3e65ef94051
bcy <1250030585@qq.com>
2018-03-27 21:40:35 +0800

    2018.3.27

---
+++ newhomework_6/BITMAPINFO_1.h
@@ -0,0 +1,49 @@
+#ifndef BITMAPINFO_H
+#define BITMAPINFO_H
+
+typedef unsigned char BYTE;
+typedef unsigned short WORD;
+typedef unsigned long DWORD;
+typedef long LONG;
+
+//bitmap file header
+typedef struct tagBITMAPFILEHEADER {
+    //WORD bfType;		//文件类型，必须是0x424D，即字符“BM”
+    DWORD bfSize;		//文件大小
+    WORD bfReserved1;	//保留字
+    WORD bfReserved2;	//保留字
+    DWORD bfoffBits;	//从文件头到实际位图数据的偏移字节数
+} BITMAPFILEHEADER;
+
+//bitmap information head
+typedef struct tagBITMAPINFOHEADER {
+    DWORD biSize;			//信息头大小
+    LONG biHeight;			//图像宽度
+    LONG biWidth;			//图像高度
+    WORD biPlanes;			//位平面数，必须为1
+    WORD biBitCount;		//像素位数
+    DWORD biCompression;	//压缩类型
+    DWORD biSizeImage;		//压缩图像大小字节数
+    LONG biXPelsPerMeter;	//水平分辨率
+    LONG biYPelsPerMeter;	//垂直分辨率
+    DWORD biClrUsed;		//位图实际用到的色彩数
+    DWORD biClrImportant;	//本位图中重要的色彩数
+} BITMAPINFOHEADER;
+
+//bitmap palette/color table
+typedef struct tagRGBQUAD {
+    BYTE rgbBlue;
+    BYTE rgbGreen;
+    BYTE rgbRed;
+    BYTE rgbReserved;
+} RGBQUAD;
+
+//bitmap data
+typedef struct tagIMAGEDATA {
+    BYTE blue;
+    //BYTE green;
+    //BYTE red;
+} IMAGEDATA;
+
+#endif
+
--- newhomework_6/new.cpp
+++ newhomework_6/new.cpp
@@ -5,12 +5,14 @@
 
 using namespace std;
 
-#define LENGTH_NAME_BMP 100		//bmp file name 
-
+#define LENGTH_NAME_BMP 100     //bmp file name
+IMAGEDATA *imagedata = NULL;
+IMAGEDATA *imagedataOut = NULL;
 BITMAPFILEHEADER strHead;
 BITMAPINFOHEADER strInfo;
 RGBQUAD strPalette[256];
-
+int width;
+int height;
 void showBmpHead(BITMAPFILEHEADER pBmpHead) {
     cout << "the bitmap header file: " << endl;
     cout << "the size of bitmap: " << pBmpHead.bfSize << endl;
@@ -33,18 +35,12 @@ void showBmpInfoHead(tagBITMAPINFOHEADER pBmpInfoHead) {
     cout << "the number of used colors: " << pBmpInfoHead.biClrUsed << endl;
     cout << "important colors: " << pBmpInfoHead.biClrImportant << endl;
 }
+int LoadBmpFile(char *strFile) {
 
-int main() {
-    char strFile[LENGTH_NAME_BMP] = "/Users/bcy/lena.bmp";
-    IMAGEDATA *imagedata = NULL;
-    IMAGEDATA *imagedataOut = NULL;
-    int width;
-    int height;
-    FILE *fp, *fpo;
+    FILE *fp;
     fp = fopen(strFile, "rb");
-
     if( fp != NULL ) {
-        WORD bfType;	//the type of the file
+        WORD bfType;    //the type of the file
         fread(&bfType, sizeof(WORD), 1, fp);
 
         if( 0x4d42 != bfType ) {
@@ -69,15 +65,14 @@ int main() {
         //width = (width * sizeof(IMAGEDATA) + 3) /4 * 4;
         width  = ((width % 4 == 0  ? width : width + (4 - (width % 4))) * strInfo.biBitCount) / 8;
         imagedata = (IMAGEDATA *)malloc( width * height );
-        imagedataOut = (IMAGEDATA *)malloc( width * height * sizeof(imagedata));
+
 
         for(int j = 0; j < height; j++) {
             for(int i = 0 ; i < width; i++) {
                 (*(imagedata + j * width + i)).blue = 0;
-                (*(imagedataOut + j * width + i)).blue = 0;
+                //(*(imagedataOut + j * width + i)).blue = 0;
             }
         }
-
         //读出图片的像素数据
         fread(imagedata, sizeof(struct tagIMAGEDATA) * width, height, fp);
         fclose(fp);
@@ -85,15 +80,65 @@ int main() {
         cout << "File open error!" << endl;
         return 0;
     }
+}
 
-    for(int j = 0; j < height; j++) {
-        for(int i = 0 ; i < width; i++) {
-            *(imagedataOut + j * width + i) = *(imagedata + j * width + i);
+int RotateImg() {
+    double theta;
+    int nwidth, nheight;
+    double kx, ky, ox, oy;
+    printf("theta:\n");
+    scanf("%lf", &theta);
+    double hudu = 3.1415 * theta / 180;
+    nheight = (int)(width * sin(hudu) + height * cos(hudu));
+    nwidth = (int)(width * cos(hudu) + height * sin(hudu));
+    printf("%d,%d\n", nheight, nwidth);
+    double xyinzi = 1.0 * nwidth / width ;
+    printf("cos:%lf\n", sin(hudu));
+    double yyinzi = 1.0 * nheight / height;
+
+    imagedataOut = (IMAGEDATA *)malloc( width * height * sizeof(imagedata));
+
+    for(int j = 0; j < nheight; j++) {
+        for(int i = 0 ; i < nwidth; i++) {
+            (*(imagedataOut + j * width + i)).blue = 0;
         }
     }
+    int tempx = 0;
+    int tempy = 0;
+    int color[2][2];
+    double sinx = sin(hudu);
+    double cosx = cos(hudu);//提前算出来提高效率
+    for(int j = 0; j < height; j++) {
+        for(int i = 0 ; i < width; i++) {
+            kx = (i * xyinzi - nwidth / 2) * cosx + (j * yyinzi - nheight / 2) * sinx + 128;
+            ky = -(i * xyinzi - nwidth / 2) * sinx + (j * yyinzi - nheight / 2) * cosx + 128;
+            if((kx >= 0) && (ky >= 0) && (kx < width) && (ky < height)) {
+                tempx = (int) kx;
+                ox = kx - tempx;
+                tempy = (int) ky;
+                oy = ky - tempy;
+                color[0][0] = (*(imagedata + tempy * width + tempx)).blue ;
+                color[1][0] = (*(imagedata + tempy * width + tempx + 1)).blue ;
+                color[0][1] = (*(imagedata + (tempy + 1) * width + tempx)).blue ;
+                color[1][1] = (*(imagedata + (tempy + 1) * width + tempx + 1)).blue;
+                double answer = (1 - ox) * (1 - oy) * color[0][0] + ox * (1 - oy) * color[1][0] +
+                                (1 - ox) * oy * color[0][1] + ox * oy * color[1][1];
+                int fi = (int) answer;
+                (*(imagedataOut + j * width + i)).blue = fi;
+            } else {
+                (*(imagedataOut + j * width + i)).blue = 255;
+            }
+        }
+    }
+    return 0;
+}
 
-    if( (fpo = fopen("out.bmp", "wb")) == NULL) {
+int SaveImg(char *outpath) {
+    FILE *fpo;
+    if( (fpo = fopen(outpath, "wb")) == NULL) {
         cout << "Create the bmp file error!" << endl;
+        free(imagedata);
+        free(imagedataOut);
         return 0;
     }
 
@@ -114,16 +159,21 @@ int main() {
 
     //保存像素数据
     for(int j = 0; j < height; j++) {
-        for(int i = 0; i < width; i++) {
-            fwrite(&((*(imagedataOut + j * width + i)).blue), sizeof(BYTE), 1, fpo);
-        }
+        fwrite(&((*(imagedataOut + j * width)).blue), sizeof(BYTE) * width, 1, fpo);
     }
 
     //getchar();
 
     fclose(fpo);
+}
 
-    delete [] imagedata;
-    delete [] imagedataOut;
 
+int main() {
+    char path[LENGTH_NAME_BMP] = "lena.bmp";
+    char wirpath[LENGTH_NAME_BMP] = "out.bmp";
+    LoadBmpFile(path);
+    RotateImg();
+    SaveImg(wirpath);
+    free(imagedata);
+    free(imagedataOut);
 }
